5d0dba1f70cf444b0209a9ea0602cded66407bd9
Guilherme Soares <soaresgui.dev@gmail.com>
2024-10-23 00:43:28 -0400

    fix: Adressing some issues

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -37,6 +37,9 @@ return {
         "NeoTreeNormalNC",
       }, -- table: additional groups that should be cleared
       exclude_groups = {}, -- table: groups you don't want to clear
+      -- function: code to be executed after highlight groups are cleared
+      -- Also the user event "TransparentClear" will be triggered
+      on_clear = function() end,
     })
   end,
 }
